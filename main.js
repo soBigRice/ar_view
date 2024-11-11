c4ee08d6ec9a469fff5b7238221e62fc20eb9cfd
MrRice <1246333567@qq.com>
2024-11-11 15:35:48 +0800

    先更新一个版本

--- main.js
+++ main.js
@@ -1,24 +1,64 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/*
+ * @Author: MrRice
+ * @Date: 2024-11-08 11:43:00
+ * @LastEditors: MrRice 1246333567@qq.com
+ * @LastEditTime: 2024-11-11 15:35:30
+ * @FilePath: /vr_viewer/main.js
+ * @Description:
+ * -----小舟从此逝，江海寄余生----
+ * Copyright (c) 2024 by 米大饭, All Rights Reserved.
+ */
+import "./style.css";
+import * as THREE from "three";
+import * as ZapparThree from "@zappar/zappar-threejs";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const manager = new ZapparThree.LoadingManager();
+let scene = new THREE.Scene();
 
-setupCounter(document.querySelector('#counter'))
+const container = document.getElementById("app");
+
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+const camera = new ZapparThree.Camera();
+// Add the below line for consistency
+camera.backgroundTexture.encoding = THREE.sRGBEncoding;
+scene.background = camera.backgroundTexture;
+container.appendChild(renderer.domElement);
+
+ZapparThree.glContextSet(renderer.getContext());
+
+ZapparThree.permissionRequestUI().then((granted) => {
+  if (granted) camera.start();
+  else ZapparThree.permissionDeniedUI(); 
+});
+
+renderer.autoClear = false;
+let imageTracker = new ZapparThree.ImageTrackerLoader(manager).load(
+  "/public/locate.zpt"
+);
+let trackerGroup = new ZapparThree.ImageAnchorGroup(camera, imageTracker);
+scene.add(trackerGroup);
+
+//创建一个小盒子再这里
+const box = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+trackerGroup.add(box); //将盒子添加到trackerGroup中
+
+window.addEventListener("resize", () => {
+  renderer.setSize(container.clientWidth, container.clientHeight);
+
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+});
+function animate() {
+  requestAnimationFrame(animate);
+  camera.updateFrame(renderer);
+  renderer.render(scene, camera);
+  renderer.clear();
+}
+
+animate();
